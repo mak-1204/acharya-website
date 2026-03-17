a304e12b6c80241cb0cfc91f633b2784db533d67
Akshay Kumar <akshay.muralibabu@gmail.com>
2026-03-17 18:01:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "acharya-ascent-1-2996421-418ce",
+  "appId": "1:234469229180:web:dbf41e01e6f4f1b49bfff9",
+  "apiKey": "AIzaSyCMpshD8U2ItSAEFf98flMysglrMI9zzdA",
+  "authDomain": "acharya-ascent-1-2996421-418ce.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "234469229180"
+};
